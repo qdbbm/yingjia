e4e605dcebf81f18b36e784664b762681bc49285
zzw118 <2238662003@qq.com>
2019-03-19 18:19:22 +0800

    微聊详情、经纪人、我的房源

---
+++ pages/jingjiren/jingjiren.js
@@ -0,0 +1,150 @@
+// pages/jingjiren/jingjiren.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    pictures: [],
+    dcgsIndex: 0,
+    dcgsArray: ['地产公司1', '地产公司2'],
+    zwIndex: 0,
+    zwArray: ['职位1', '职位2'],
+    zydqIndex1: 0,
+    zydqArray1: ['地区1', '地区2'],
+    zydqIndex2: 0,
+    zydqArray2: ['地区1', '地区2']
+  },
+
+  bindPickerChange(e) {
+    this.setData({
+      dcgsIndex: e.detail.value
+    })
+  },
+
+  bindPickerChange2(e) {
+    this.setData({
+      zwIndex: e.detail.value
+    })
+  },
+
+  bindPickerChange3(e) {
+    this.setData({
+      zydqIndex1: e.detail.value
+    })
+  },
+
+  bindPickerChange4(e) {
+    this.setData({
+      zydqIndex2: e.detail.value
+    })
+  },
+
+  /**
+   * 上传图片
+   */
+  uploadImg: function () {
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      success: function (res) {
+        var tempFilePath = res.tempFilePaths[0]
+        wx.saveFile({
+          tempFilePath: tempFilePath,
+          success: function (res) {
+            var savedFilePath = res.savedFilePath
+            console.log(savedFilePath)
+            that.setData({
+              pictures: that.data.pictures.concat(savedFilePath)
+            })
+            wx.setStorage({
+              key: 'gallery',
+              data: that.data.pictures
+            })
+          }
+        })
+      }
+    })
+  },
+
+  /**
+   * 预览图片
+   */
+  previewImage: function (e) {
+    var data = e.currentTarget.dataset
+    var index = data.index
+    var that = this
+    wx.previewImage({
+      current: that.data.pictures[index], // 当前显示图片的链接，不填则默认为 urls 的第一张
+      urls: that.data.pictures
+    })
+  },
+
+  /**
+   * 删除图片
+   */
+  tapDelImage: function (e) {
+    var pictures = this.data.pictures;
+    var index = e.currentTarget.dataset.index;
+    pictures.splice(index, 1);
+    this.setData({
+      pictures: pictures
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/myHouse/myHouse.js
@@ -0,0 +1,99 @@
+// pages/myHouse/myHouse.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    houseList: [
+      {
+        url: '',
+        imgUrl: 'http://images.zhengzw.com/yingjia/fy-img.jpg',
+        tle: '横琴百家达星际广场',
+        address: '香洲-唐家',
+        txt: '四室 建面54-350㎡',
+        price: '3000'
+      },
+      {
+        url: '',
+        imgUrl: 'http://images.zhengzw.com/yingjia/fy-img.jpg',
+        tle: '横琴百家达星际广场',
+        address: '香洲-唐家',
+        txt: '四室 建面54-350㎡',
+        price: '3000'
+      },
+      {
+        url: '',
+        imgUrl: 'http://images.zhengzw.com/yingjia/fy-img.jpg',
+        tle: '横琴百家达星际广场',
+        address: '香洲-唐家',
+        txt: '四室 建面54-350㎡',
+        price: '3000'
+      },
+      {
+        url: '',
+        imgUrl: 'http://images.zhengzw.com/yingjia/fy-img.jpg',
+        tle: '横琴百家达星际广场',
+        address: '香洲-唐家',
+        txt: '四室 建面54-350㎡',
+        price: '3000'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
