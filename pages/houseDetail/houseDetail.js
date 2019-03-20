49bfdd3e19baa827f4a8ccfa04e730cb7f5b77dc
qdbbm <582877596@qq.com>
2019-03-20 23:25:37 +0800

    房源详情

--- pages/houseDetail/houseDetail.js
+++ pages/houseDetail/houseDetail.js
@@ -1,4 +1,10 @@
 // pages/houseDetail/houseDetail.js
+
+//获取应用实例
+const app = getApp()
+
+const utils = require("../../utils/util.js")
+
 Page({
 
   /**
@@ -14,13 +20,55 @@ Page({
       address: '中源大道',
       price: 3000,
       jingjin: 3000
-    }
+    },
+    tabTle: [
+      '基础信息',
+      '结佣规则',
+      '专家解读'
+    ],
+    current: 'jcxx',
+    zjList: [
+      {
+        imgUrl: 'http://images.zhengzw.com/yingjia/zj-img.jpg',
+        name: '耿专家',
+        tag: '金牌分析师',
+        job: '房产顾问丨哈尔滨房产分析师',
+        txt: '富力城楼盘频道，提供哈尔滨富力城 房价，楼盘户型，楼盘产权年限，物业费，开发商，售楼电话，绿化率，周边配套，商场，超市，学校，医院，银行，周边地图交通等全方位楼盘信息，富力城开盘交房时……'
+      },
+      {
+        imgUrl: 'http://images.zhengzw.com/yingjia/zj-img.jpg',
+        name: '耿专家',
+        tag: '金牌分析师',
+        job: '房产顾问丨哈尔滨房产分析师',
+        txt: '富力城楼盘频道，提供哈尔滨富力城 房价，楼盘户型，楼盘产权年限，物业费，开发商，售楼电话，绿化率，周边配套，商场，超市，学校，医院，银行，周边地图交通等全方位楼盘信息，富力城开盘交房时……'
+      }
+    ],
+    phoneNumber: '13888889999',
+    love: false
   },
 
   hdTabChange: function (e) {
     let index = e.currentTarget.dataset.index;
     this.setData({
       currentHdTabIndex: index
+    })
+  },
+
+  tabChange: function (e) {
+    let current = e.currentTarget.dataset.current;
+    this.setData({
+      current: current
+    })
+  },
+
+  call: function (e) {
+    let phoneNumber = e.currentTarget.dataset.phone;
+    utils.call(phoneNumber);
+  },
+
+  tapLove: function (e) {
+    this.setData({
+      love: !this.data.love
     })
   },
 
